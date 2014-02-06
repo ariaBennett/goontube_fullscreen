9974633b145a78853d3b3654263fa8b166bd1749
AriaB <ariasayshi@gmail.com>
2014-02-05 16:24:26 -0800

    Script now adds a fullscreen button which can toggle between normal mode and fullscreen mode.

--- goontubeFullscreen.js
+++ goontubeFullscreen.js
@@ -4,7 +4,24 @@
 // gtfo stands for "goontube fullscreen option"
 window.gtfo = {};
 
+// Namespace globals
+gtfo.enabled = false;
+
 // Function Section.
+gtfo.each = function each(obj, callback) {
+  // A simple each implementation, works on arrays and objects
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      callback(obj[i]);
+    }
+  }
+  else {
+    for (var i = 0; i < Object.keys(obj).length; i++) {
+      callback(obj[Object.keys(obj)[i]]);
+    };
+  }
+};
+
 gtfo.getVideo = function getVideo() {
   // This returns the element of the current video player.
   var video;
@@ -69,11 +86,12 @@ gtfo.getSlider = function getSlider() {
 gtfo.adjustStyles = function adjustStyles() {
   // This function makes property adjustments to elements that already
   // exist on goontu.be.
-  var chat = gtfo.getChatClass();
   window.scrollTo(0, 0);
   document.body.setAttribute("style",
     "overflow : hidden;"
   );
+
+  var chat = gtfo.getChatClass();
   chat.style.position = "fixed";
   chat.style.top = 0;
   chat.style.right = 0;
@@ -194,7 +212,7 @@ gtfo.attachVideoTypeChangeDetection = function attachVideoTypeChangeDetection() 
   // if it finds any it resets the UI.
 
   function checkVideoChange() {
-    if (document.getElementById("ytwrapper").childElementCount > 0) {
+    if (gtfo.enabled === true && document.getElementById("ytwrapper").childElementCount > 0) {
       gtfo.moveElements();
       gtfo.resizeVideo();
       gtfo.currentPlayer = gtfo.getVideo().id;
@@ -214,16 +232,82 @@ gtfo.fixFixButton = function fixFixButton() {
   };
 };
 
+gtfo.storeDefaultStyles = function storeDefaultStyles() {
+  // This stores the styles of relevant elements so that we
+  // can restore them later.
+  gtfo.defaultStyles = {};
+  function storeStyle(name, style) {
+    gtfo.defaultStyles[name] = JSON.parse(JSON.stringify(style));
+  }
+
+  storeStyle("body", document.body.style);
+  storeStyle("chatClass", gtfo.getChatClass().style);
+  storeStyle("chatId", gtfo.getChatId().style);
+  storeStyle("chatList", document.getElementById("chat_list").style);
+  storeStyle("video", gtfo.getVideo().style);
+  storeStyle("controls", gtfo.getControls().style);
+  storeStyle("chatInput", document.getElementById("cin").style);
+  storeStyle("controlsLikeHate", document.getElementById("likehate").style);
+  storeStyle("cams", document.getElementsByClassName("cameras")[0].style);
+};
+
+gtfo.restoreStyles = function restoreStyles() {
+  function restoreStyle(element, defaultStyle) {
+    var elementStyle = element.style;
+    var keys = Object.keys(defaultStyle);
+    gtfo.each(keys, function(key){
+      elementStyle[key] = defaultStyle[key];
+    });
+  }
+
+  restoreStyle(document.body, gtfo.defaultStyles["body"]);
+  restoreStyle(gtfo.getChatClass(), gtfo.defaultStyles["chatClass"]);
+  restoreStyle(gtfo.getChatId(), gtfo.defaultStyles["chatId"]);
+  restoreStyle(document.getElementById("chat_list"), gtfo.defaultStyles["chatList"]);
+  restoreStyle(gtfo.getVideo(), gtfo.defaultStyles["video"]);
+  restoreStyle(gtfo.getControls(), gtfo.defaultStyles["controls"]);
+  restoreStyle(document.getElementById("cin"), gtfo.defaultStyles["chatInput"]);
+  restoreStyle(document.getElementById("likehate"), gtfo.defaultStyles["controlsLikeHate"]);
+  restoreStyle(document.getElementsByClassName("cameras")[0], gtfo.defaultStyles["cams"]);
+};
+
 gtfo.restoreNormalLayout = function restoreNormalLayout() {
   // this undos fullscreen mode and returns everything back to
   // normal.
 
+  // Turn off enabled flag
+  gtfo.enabled = false;
+
   // Remove added events
   window.onresize = "";
   document.getElementById("fixmedia").onclick = "";
+  var fixButton = document.getElementById("fixmedia");
+  fixButton.onClick = "";
+
+  // Restore default element sizes
+  var video = gtfo.getVideo();
+  video.width = "550px";
+  video.height = "320px";
 
   // Move elements back into place.
+  // Video
   document.getElementById("ytwrapper").appendChild(gtfo.getVideo());
+  // Controls
+  document.getElementById("playlistcontrols").appendChild(gtfo.getControls());
+  // Chat
+  var stageOuter = document.getElementsByClassName("st-vanilla-stage-ct")[0];
+  var stageInner= document.getElementsByClassName("stage")[0];
+  stageOuter.insertBefore(gtfo.getChatClass(), stageInner.nextSibling);
+
+  // Restore the normal text to the controls.
+  var controlText = gtfo.getControls("text");
+  var controlTextCams = controlText[0];
+  var controlTextAdd = controlText[1];
+  var controlTextSkip = controlText[2];
+  var controlTextFix = controlText[3];
+  controlTextCams.textContent = "enable webcams";
+  controlTextAdd.textContent = "add vid";
+  controlTextFix.textContent = "fix media";
   
   // unhide hidden elements
   document.getElementById("chat_users").style.display = "inline";
@@ -232,18 +316,38 @@ gtfo.restoreNormalLayout = function restoreNormalLayout() {
   gtfo.getSlider().remove();
 
   // reset styles of elements to normal
-  gtfo.getVideo().width = gtfo.getVideo().parentElement.width;
-  gtfo.getVideo().height = gtfo.getVideo().parentElement.height;
-
+  gtfo.restoreStyles();
 };
 
+gtfo.run = function run() {
+  gtfo.hideExtraElements();
+  gtfo.moveElements();
+  gtfo.adjustStyles();
+  gtfo.resizeVideo();
+  gtfo.autoResize();
+  gtfo.createTransparencySlider();
+  gtfo.attachVideoTypeChangeDetection();
+  gtfo.fixFixButton();
+};
+
+gtfo.addFullscreenButton = function addFullscreenButton(){
+  var buttonFullscreen = document.getElementById("webcamtog").cloneNode(true);
+  var buttonFullscreenChild = buttonFullscreen.children[0];
+  buttonFullscreen.id = "fullscreentog";
+  buttonFullscreenChild.textContent = "fullscreen";
+  buttonFullscreen.onclick = function() {
+    if (gtfo.enabled === false) {
+      gtfo.enabled = true;
+      gtfo.run();
+    }
+    else {
+      gtfo.enabled = false;
+      gtfo.restoreNormalLayout();
+    }
+  };
+  gtfo.getControls().appendChild(buttonFullscreen);
+};
 
 // Run Section.
-gtfo.hideExtraElements();
-gtfo.moveElements();
-gtfo.adjustStyles();
-gtfo.resizeVideo();
-gtfo.autoResize();
-gtfo.createTransparencySlider();
-gtfo.attachVideoTypeChangeDetection();
-gtfo.fixFixButton();
+gtfo.addFullscreenButton();
+gtfo.storeDefaultStyles();
